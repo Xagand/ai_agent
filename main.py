21617732ddf39d8eb1643cece0eb15e438810b65
Xagand <dariasport21@gmail.com>
2025-07-21 14:44:46 -0500

    First stage complete

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import os, sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 def main():
-    print("Hello from ai-agent!")
+    
+    
+    load_dotenv()
+    key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=key)
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(response.text)
+        print(f"User prompt: {user_prompt}'\n",
+              f"Prompt tokens: {response.usage_metadata.prompt_token_count}'\n",
+              f"Response tokens: {response.usage_metadata.candidates_token_count}")   
+    else:
+        print(response.text)
+    
 
 
 if __name__ == "__main__":
